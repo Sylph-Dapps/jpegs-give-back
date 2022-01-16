f0af573702d61fc3ccb1a5f3f95b219d25beae8f
Michael VanDaniker <mvandani@gmail.com>
2022-01-16 09:54:33 -0500

    Adds addresses.js

---
+++ packages/react-app/src/contracts/addresses.js
@@ -0,0 +1,15 @@
+const beneficiary = "0x0000000000000000000000000000000000000000";
+const punksAddress = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB";
+const baycAddress = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
+const maycAddress = "0x60E4d786628Fea6478F785A6d7e704777c86a7c6";
+const bakcAddress = "0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623";
+const baccAddress = "0x22c36BfdCef207F9c0CC941936eff94D4246d14A";
+
+module.exports = {
+  beneficiary,
+  punksAddress,
+  baycAddress,
+  maycAddress,
+  bakcAddress,
+  baccAddress,
+};
